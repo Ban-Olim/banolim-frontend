805b7fac01b7fc86c423c8758d7552374181a56f
Navi <masimello0001@gmail.com>
2026-03-16 15:13:05 +0900

    Update page.tsx

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Image from "next/image";
-import MenuCard from "../components/MenuCard";
+import MenuCard from "../../components/MenuCard";
 
 const Cloud = ({
   className,
